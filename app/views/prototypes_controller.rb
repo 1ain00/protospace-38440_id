0aba67c8d8c35a7cee57945a3705f6a362015e05
1ain00 <ponpon.krr-n@i.softbank.jp>
2022-09-09 19:11:27 +0900

    投稿者以外が編集ページに遷移でいないようにeditアクションを修正

--- app/views/prototypes_controller.rb
+++ app/views/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: :show
-
+  before_action :move_to_index, except: :index
 
   def index
     @prototypes = Prototype.all
@@ -27,10 +27,6 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      current_user.id = @prototype.user_id
-      redirect_to action: :index
-    end
   end
 
   def update
@@ -53,4 +49,13 @@ private
   def prototype_params
     params.require(:prototype).permit([:title, :catch_copy, :concept, :image]).merge(user_id: current_user.id)
   end
+  
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in?
+      current_user.id = @prototype.user_id
+      redirect_to action: :index
+    end
+  end 
+
 end

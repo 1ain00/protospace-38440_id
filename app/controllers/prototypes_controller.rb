970ed39966503d3a1f0ad662b5b86f5a5fdb39e2
1ain00 <ponpon.krr-n@i.softbank.jp>
2022-09-10 20:07:58 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,9 +1,10 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: :show
-
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :move_to_index, except: [:index, :show]
 
   def index
     @prototypes = Prototype.all
+
   end
 
   def new
@@ -22,20 +23,16 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id]) 
     @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
+    @comments = @prototype.comments.includes(:user, :prototype)
   end
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      current_user.id = @prototype.user_id
-      redirect_to action: :index
-    end
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
       render :edit
@@ -51,6 +48,13 @@ class PrototypesController < ApplicationController
 private
 
   def prototype_params
-    params.require(:prototype).permit([:title, :catch_copy, :concept, :image]).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end 
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,8 @@
 class UsersController < ApplicationController
 
+
   def show
-    @user = User.find(params[:id]) 
-    @user_name = current_user.name
-    @profile = current_user.profile
-    @occupation = current_user.occupation
-    @position = current_user.position
-    @prototypes = current_user.prototypes
-  end
+    @user = User.find(params[:id])
+    @prototypes = Prototype.all
+end
 end
